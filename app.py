dc280946f403e71a791bd36c806ca436bb8a7352
tushar8871 <tusharkhachane6@gmail.com>
2021-10-27 05:18:01 +0000

    Modify

--- app.py
+++ app.py
@@ -67,5 +67,5 @@ def predict():
 
 if __name__ == "__main__":
     # load pretrained model as clf
-    clf = joblib.load("./model_data/boston_housing_prediction.joblib")
+    clf = joblib.load("./models/boston_housing_prediction.joblib")
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
